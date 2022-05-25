60f28e98ee7bfdf68834dbc5e57d4cc57e24c9f0
Kules <kul.artem@bk.ru>
2022-05-26 00:53:26 +0300

    del class

--- linear-regression.py
+++ linear-regression.py
@@ -3,44 +3,46 @@ import matplotlib.pyplot as plt
 import random
 import math
 
+b0 = 0
+b1 = 0
 
-class LinearRegression:
-    b0 = 0
-    b1 = 0
 
-    def fit(self, X, Y):
-        n = len(X)
-        self.b0 = (sum(Y) * sum(X ** 2) - sum(X) * sum(X * Y)) / (n * sum(X ** 2) - sum(X) ** 2)
-        self.b1 = (n * sum(X * Y) - sum(X) * sum(Y)) / (n * sum(X ** 2) - sum(X) ** 2)
+def fit(X, Y):
+    n = len(X)
+    b0 = (sum(Y) * sum(X ** 2) - sum(X) * sum(X * Y)) / (n * sum(X ** 2) - sum(X) ** 2)
+    b1 = (n * sum(X * Y) - sum(X) * sum(Y)) / (n * sum(X ** 2) - sum(X) ** 2)
+    return b0, b1
 
-    def predict(self, X):
-        print(f'{self.b0} + {self.b1} * X')
-        return self.b0 + self.b1 * X
 
-    def getCharacteristics(self, X, Y):
-        characteristics = {}
-        Y_mean = np.mean(Y)
-        X_mean = np.mean(X)
-        Y_pred = self.predict(X)
-        n = len(X)
-        characteristics['E_sko'] = sum((Y - Y_pred) ** 2) / (len(X) - 2)
-        characteristics['E_st'] = math.sqrt(characteristics['E_sko'])
-        characteristics['Q'] = sum((Y - Y_mean) ** 2)
-        characteristics['Qr'] = sum((Y_pred - Y_mean) ** 2)
-        characteristics['Qe'] = sum((Y - Y_pred) ** 2)
-        characteristics['det_koef'] = characteristics['Qr'] / characteristics['Q']
-        cov = sum((X - X_mean) * (Y - Y_mean)) / n
-        sigma_x = math.sqrt(sum((X - X_mean) ** 2) / (n - 1))
-        sigma_y = math.sqrt(sum((Y - Y_mean) ** 2) / (n - 1))
-        characteristics['kor_koef'] = cov / (sigma_x * sigma_y)
+def predict(X):
+    print(f'{b0} + {b1} * X')
+    return b0 + b1 * X
 
-        print(f"Еско = {characteristics['E_sko']}")
-        print(f"Ест = {characteristics['E_st']}")
-        print(f"Q = {characteristics['Q']}")
-        print(f"Qr = {characteristics['Qr']}")
-        print(f"Qe = {characteristics['Qe']}")
-        print(f"Коэффициент детерминации = {characteristics['det_koef']}")
-        print(f"Коэффициент корреляции = {characteristics['kor_koef']}")
+
+def getCharacteristics(X, Y):
+    characteristics = {}
+    Y_mean = np.mean(Y)
+    X_mean = np.mean(X)
+    Y_pred = predict(X)
+    n = len(X)
+    characteristics['E_sko'] = sum((Y - Y_pred) ** 2) / (len(X) - 2)
+    characteristics['E_st'] = math.sqrt(characteristics['E_sko'])
+    characteristics['Q'] = sum((Y - Y_mean) ** 2)
+    characteristics['Qr'] = sum((Y_pred - Y_mean) ** 2)
+    characteristics['Qe'] = sum((Y - Y_pred) ** 2)
+    characteristics['det_koef'] = characteristics['Qr'] / characteristics['Q']
+    cov = sum((X - X_mean) * (Y - Y_mean)) / n
+    sigma_x = math.sqrt(sum((X - X_mean) ** 2) / (n - 1))
+    sigma_y = math.sqrt(sum((Y - Y_mean) ** 2) / (n - 1))
+    characteristics['kor_koef'] = cov / (sigma_x * sigma_y)
+
+    print(f"Еско = {characteristics['E_sko']}")
+    print(f"Ест = {characteristics['E_st']}")
+    print(f"Q = {characteristics['Q']}")
+    print(f"Qr = {characteristics['Qr']}")
+    print(f"Qe = {characteristics['Qe']}")
+    print(f"Коэффициент детерминации = {characteristics['det_koef']}")
+    print(f"Коэффициент корреляции = {characteristics['kor_koef']}")
 
 
 def generateData(amount=30):
@@ -58,9 +60,9 @@ def generateData(amount=30):
     return X, Y
 
 
-def plotGraph(lr, X_train, Y_train, X_test, Y_test):
+def plotGraph(X_train, Y_train, X_test, Y_test):
     x_pred = np.arange(-10, +10)
-    y_pred = lr.predict(x_pred)
+    y_pred = predict(x_pred)
     plt.plot(x_pred, y_pred, color='red', label='regression')
     plt.scatter(X_train, Y_train, label='train', s=3)
     plt.scatter(X_test, Y_test, label='test', s=3)
@@ -75,8 +77,7 @@ train_quantity = int(len(X) * test_percent)
 X_train, Y_train = X[:train_quantity], Y[:train_quantity]
 X_test, Y_test = X[train_quantity:], Y[train_quantity:]
 
-lr = LinearRegression()
-lr.fit(X_train, Y_train)
-lr.getCharacteristics(X, Y)
+b0, b1 = fit(X_train, Y_train)
+getCharacteristics(X, Y)
 
-plotGraph(lr, X_train, Y_train, X_test, Y_test)
+plotGraph(X_train, Y_train, X_test, Y_test)
