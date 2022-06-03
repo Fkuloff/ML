000b91cfe6e6e0f1c7a3d737b116fd7b0f8d8637
Kules <kul.artem@bk.ru>
2022-06-04 02:44:37 +0300

    5 fix + dataset

--- bayesian-classifier.py
+++ bayesian-classifier.py
@@ -6,28 +6,33 @@ df = pd.read_excel('bl.xlsx')
 print(df)
 
 # Параметры человека, у которого хотим предсказать статус визы
-FIELDS = ['Работа', 'Заработок', 'Цель_поездки', 'Английский_язык']
+FIELDS = ['Опыт_разработки', 'Уровень_знаний', 'Претендуемая_позиция', 'Английский_язык']
 X = [1, 'Средний', 'Д', 1]
 
 approved_count = list(df['Статус']).count('Одобрено')
 refused_count = list(df['Статус']).count('Отказано')
 approved_p = approved_count / len(df)
 refused_p = refused_count / len(df)
-print(f'P(Статус=Отказано) = {refused_p}, P(Статус=Одобрено) = {approved_p}')
+print(f'\nP(Статус=Отказано) = {refused_p}, P(Статус=Одобрено) = {approved_p}')
 
 X_approve_p = 1
 X_refuse_p = 1
 for param in enumerate(X):
+    print(param)
     print(
-        f"P({FIELDS[param[0]]}={param[1]} | Статус=Одобрено) = {len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count}")
+        f"P({FIELDS[param[0]]}={param[1]} | Статус=Одобрено) ="
+        f" {len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count}"
+    )
     X_approve_p *= len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count
 
     print(
-        f"P({FIELDS[param[0]]}={param[1]} | Статус=Отказано) = {len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count}")
+        f"P({FIELDS[param[0]]}={param[1]} | Статус=Отказано) ="
+        f" {len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count}"
+    )
     X_refuse_p *= len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count
 
-print()
-print(f'P(X | Статус=Одобрено) = {X_approve_p}')
+
+print(f'\nP(X | Статус=Одобрено) = {X_approve_p}')
 print(f'P(X | Статус=Отказано) = {X_refuse_p}')
 a = X_approve_p * approved_p
 b = X_refuse_p * refused_p
