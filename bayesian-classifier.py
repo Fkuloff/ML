e91228b72beeb5f094953048a84d2eb1ca9ca826
Kules <kul.artem@bk.ru>
2022-06-04 03:50:07 +0300

    round values

--- bayesian-classifier.py
+++ bayesian-classifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_excel('bl.xlsx')
-print(df)
+print(df.to_string())
 
 FIELDS = ['Опыт_разработки', 'Уровень_знаний', 'Претендуемая_позиция', 'Английский_язык']
 X = [1, 'Средний', 'Д', 1]
@@ -10,30 +10,30 @@ approved_count = list(df['Статус']).count('Одобрено')
 refused_count = list(df['Статус']).count('Отказано')
 approved_p = approved_count / len(df)
 refused_p = refused_count / len(df)
-print(f'\nP(Статус=Отказано) = {refused_p}, P(Статус=Одобрено) = {approved_p}')
+print(f'\nP(Статус=Отказано) = {round(refused_p, 3)}, P(Статус=Одобрено) = {round(approved_p, 3)}')
 
 X_approve_p = 1
 X_refuse_p = 1
 for param in enumerate(X):
     print(
         f"P({FIELDS[param[0]]}={param[1]} | Статус=Одобрено) ="
-        f" {len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count}"
+        f" {round(len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count, 3)}"
     )
     X_approve_p *= len(df[(df['Статус'] == 'Одобрено') & (df[FIELDS[param[0]]] == param[1])]) / approved_count
 
     print(
         f"P({FIELDS[param[0]]}={param[1]} | Статус=Отказано) ="
-        f" {len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count}"
+        f" {round(len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count, 3)}"
     )
     X_refuse_p *= len(df[(df['Статус'] == 'Отказано') & (df[FIELDS[param[0]]] == param[1])]) / refused_count
 
-print(f'\nP(X | Статус=Одобрено) = {X_approve_p}')
-print(f'P(X | Статус=Отказано) = {X_refuse_p}')
+print(f'\nP(X | Статус=Одобрено) = {round(X_approve_p, 3)}')
+print(f'P(X | Статус=Отказано) = {round(X_refuse_p, 3)}')
 a = X_approve_p * approved_p
 b = X_refuse_p * refused_p
-print(f'P(X | Статус=Одобрено) * P(Статус=Одобрено) = {a}')
-print(f'P(X | Статус=Отказано) * P(Статус=Отказано) = {b}')
+print(f'P(X | Статус=Одобрено) * P(Статус=Одобрено) = {round(a, 3)}')
+print(f'P(X | Статус=Отказано) * P(Статус=Отказано) = {round(b, 3)}')
 
 print('Нормализованные значения:')
-print(f'Вероятность одобрения {a / (a + b)}')
-print(f'Вероятность отказа {b / (b + a)}')
+print(f'Вероятность одобрения {round(a / (a + b), 3)}')
+print(f'Вероятность отказа {round(b / (b + a), 3)}')
